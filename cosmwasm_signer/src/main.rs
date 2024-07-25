7f91f84848dd6e2158892150623a76189845f3df
BiPhan4 <biphan374@gmail.com>
2024-07-24 23:40:09 -0400

    derive private key from devnet seed phrase

--- cosmwasm_signer/src/main.rs
+++ cosmwasm_signer/src/main.rs
@@ -1,11 +1,16 @@
+use cosmos_sdk::tendermint::Error;
 use cosmrs::{
     bank::MsgSend,
     crypto::secp256k1,
     tx::{self, AccountNumber, Fee, Msg, SignDoc, SignerInfo},
     Coin,
+    bip32,
+
 };
 
-use std::{panic, str};
+use thiserror::Error;
+
+use std::{panic, str::{self, FromStr}};
 
 /// jackal chain id
 const CHAIN_ID: &str = "puppy-1";
@@ -27,6 +32,23 @@ const MEMO: &str = "test memo";
 
 fn main() {
 
-    
+    env_logger::init();
+
+    let seed = "fork draw talk diagram fragile online style lecture ecology lawn dress hat modify member leg pluck leaf depend subway grit trumpet tongue crucial stumble";
+
+    let path_str = "m/44'/0'/0'/0/0"; // placeholder
+    let path = parse_derivation_path(path_str).expect("Failed to parse derivation path");
+
+    let sender_private_key = secp256k1::SigningKey::derive_from_path(seed, &path);
+
+
     println!("Hello, world!");
 }
+
+
+// Assuming the necessary imports and module structure
+fn parse_derivation_path(path_str: &str) -> Result<bip32::DerivationPath, bip32::Error> {
+    let path = bip32::DerivationPath::from_str(path_str)?;
+    Ok(path)
+}
+
