2222f1706cbf109fde321e0e9793d46960239a98
BiPhan4 <biphan374@gmail.com>
2024-07-25 14:22:50 -0400

    make notes regarding deriving jackal account

--- cosmwasm_signer/tests/from_seed.rs
+++ cosmwasm_signer/tests/from_seed.rs
@@ -12,15 +12,28 @@ use cosmrs::crypto::secp256k1;
 mod tests {
     use super::*;
 
+    // NOTE: canine-chain definitely uses bip39 mnemonics but the address we generated below does not 
+    // match the one the chain generated
+
+    // I think we got the wrong HD path? Or perhaps it's the password? 
+
+    // Perhaps it will still broadcast as long as we fund the account?
+
     #[test]
     fn generate_key_pair_from_seed_phrase() {
 
         // NOTE: can pass some of this back into cosmwasm_signer/src/main.rs once you're done dissecting
         let seed_phrase = "fork draw talk diagram fragile online style lecture ecology lawn dress hat modify member leg pluck leaf depend subway grit trumpet tongue crucial stumble";
         let mnemonic = Mnemonic::new(seed_phrase, Default::default()).unwrap();
-        let seed = mnemonic.to_seed("password");
+        let seed = mnemonic.to_seed("password"); 
+        // NOTE: good chance canine-chain generates the seed with a different password/salt?
+        // isn't this fine though? two accounts can have the same seed phrase but if the final seed was generated differently,
+        // they will have different addresses
+        // there's no way the user will know which password/salt was used to generate the seed 
+
         let root_xprv = XPrv::new(&seed).expect("failed to get root xprv");
 
+        // NOTE: cannot rememember where to find canine-chain HD path so we're using ATOM's for now
         // ATOM HD path: m/44'/118'/0'/0/0  
         let child_path = "m/44'/118'/0'/0/0";
         // let child_xprv = XPrv::derive_from_path(&seed, &child_path.parse().expect("no child_path")).expect("no child_xprv");
@@ -31,6 +44,9 @@ mod tests {
 
         let signing_key = sender_private_key;
         let verification_key = child_xpub;
-        println!("{:?}", verification_key)
+
+        let sender_account_id = verification_key.account_id("jkl").expect("failed to get account");
+        
+        println!("{:?}", sender_account_id)
     }
 }
