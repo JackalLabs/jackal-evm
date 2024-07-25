c406a0f295c5d861942889bb199c4cbb85e6b094
Milo-Goodfellow-Work <milogoodfellowworkemail@gmail.com>
2024-07-25 09:36:09 -0400

    EVM Listener

---
+++ evm_listener/evm_listener/src/main.rs
@@ -0,0 +1,23 @@
+use web3::futures::{ future, StreamExt };
+use web3::transports::WebSocket;
+use web3::Web3;
+
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+    // Connect to the Anvil instance
+    let transport = WebSocket::new("ws://localhost:8545").await?;
+    let web3 = Web3::new(transport);
+
+    let mut sub = web3.eth_subscribe().subscribe_new_heads().await?;
+
+    (&mut sub)
+        .take(5)
+        .for_each(|x| {
+            println!("Got: {:?}", x);
+            future::ready(())
+        })
+        .await;
+
+    sub.unsubscribe().await?;
+    Ok(())
+}
