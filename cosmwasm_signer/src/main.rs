9d231bc0f5dc416dda5291e4797cf84bcebadccb
BiPhan4 <biphan374@gmail.com>
2024-07-24 23:04:01 -0400

    add config constants for cosmwasm signer to broadcast msg to jackal chain

--- cosmwasm_signer/src/main.rs
+++ cosmwasm_signer/src/main.rs
@@ -1,5 +1,32 @@
-use cosmrs::AccountId;
+use cosmrs::{
+    bank::MsgSend,
+    crypto::secp256k1,
+    tx::{self, AccountNumber, Fee, Msg, SignDoc, SignerInfo},
+    Coin,
+};
+
+use std::{panic, str};
+
+/// jackal chain id
+const CHAIN_ID: &str = "puppy-1";
+
+/// RPC port
+const RPC_PORT: u16 = 26657;
+
+/// Expected account number
+const ACCOUNT_NUMBER: AccountNumber = 1;
+
+/// Bech32 prefix for an account
+const ACCOUNT_PREFIX: &str = "jkl";
+
+/// Denom name
+const DENOM: &str = "ujkl";
+
+/// Example memo
+const MEMO: &str = "test memo";
 
 fn main() {
+
+    
     println!("Hello, world!");
 }
