4783687a3e24c91f1dac81682b7ce73d1d6f316b
BiPhan4 <biphan374@gmail.com>
2024-08-06 19:35:24 -0400

    retrieve string from event

--- evm_listener/src/main.rs
+++ evm_listener/src/main.rs
@@ -1,8 +1,9 @@
 use web3::contract::{Contract, Options};
 use web3::futures::StreamExt;
 use web3::transports::{Http, WebSocket};
-use web3::types::{FilterBuilder, H160};
-use web3::Web3;
+use web3::types::{FilterBuilder, H160, Log};
+use web3::{ethabi, Web3};
+use ethabi::{decode, ParamType};
 
 async fn create_event_data_listener(web3_socket: &Web3<WebSocket>, address: H160) -> web3::Result<tokio::task::JoinHandle<()>> {
     let filter = FilterBuilder::default()
@@ -15,13 +16,30 @@ async fn create_event_data_listener(web3_socket: &Web3<WebSocket>, address: H160
         sub.for_each(|log| async{
             match log {
                 Ok(log) => {
-                    println!("DATA: {:?}", String::from_utf8(log.data.0.to_vec()));
+                    if let Some(value) = decode_event_data(&log) {
+                        println!("Value received: {}", value);
+                    } else {
+                        println!("Failed to decode event value");
+                    }
                 }
                 Err(e) => eprintln!("Error: {:?}", e)
             }
         }).await;
     }))
 }
+
+fn decode_event_data(log: &Log) -> Option<String> {
+    // Decode only the 'value' part of the log data (skip 'sender' and 'message')
+    let decoded_data = decode(
+        &[ParamType::String], // Only decode the 'string value' part
+        &log.data.0,
+    ).ok()?;
+
+    // Extract and return the 'value' string
+    let value = decoded_data[0].clone().to_string();
+    Some(value)
+}
+
 
 async fn deploy_test_contract(web3: Web3<Http>) -> web3::Result<Contract<Http>> {
     let abi = include_bytes!("build/MockEvent.abi");
