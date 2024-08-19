a5d8698e907b0d2b456d060c6447b68c2ed5b787
BiPhan4 <biphan374@gmail.com>
2024-08-19 19:10:54 -0400

    use rpc instead of grpc query

--- relayer/src/query.rs
+++ relayer/src/query.rs
@@ -1,25 +1,35 @@
-use cosmos_sdk_proto::cosmos::auth::v1beta1::{BaseAccount, QueryAccountRequest};
-use cosmos_sdk_proto::cosmos::auth::v1beta1::query_client::QueryClient;
-use prost_types::Any;
-use tonic::transport::Channel;
-use prost::Message; // Ensure prost::Message trait is in scope
+use cosmrs::proto::prost::Message;
+use tendermint_rpc::{Client, HttpClient};
+use tendermint_rpc::endpoint::abci_query::AbciQuery;
+use cosmrs::proto::cosmos::auth::v1beta1::BaseAccount;
 use anyhow::Result;
 
-pub(crate) async fn get_account_sequence_number(address: String, grpc_url: String) -> Result<u64> {
-    println!("grpc url is: {}", grpc_url.clone());
-    let mut client = QueryClient::connect(grpc_url.clone()).await?;
-    let request = QueryAccountRequest { address };
-    let response = client.account(request).await?;
+pub(crate) async fn get_account_sequence_number(address: String, rpc_url: String) -> Result<u64> {
+    // Initialize the Tendermint RPC client
+    let client = HttpClient::new(rpc_url.as_str())?;
 
-    let any = response.into_inner().account.unwrap();
+    println!("rpc url: {}", rpc_url );
+    println!("address: {}", address );
+
+    // Prepare the ABCI query to fetch the account information
+    // need a slash in the front?
+    let query_path = format!("cosmos.auth.v1beta1.Query/Account/{}", address);
+    // NOTE: keeps returning sequence of '1'
+    println!("query path: {}", query_path );
 
 
+    // Send the query to the ABCI endpoint
+    let response: AbciQuery = client
+        .abci_query(Some(query_path), vec![], None, false)
+        .await?;
 
-    // Decode the BaseAccount manually
-    if any.type_url == "/cosmos.auth.v1beta1.BaseAccount" {
-        let base_account: BaseAccount = BaseAccount::decode(any.value.as_slice())?;
-        Ok(base_account.sequence)
-    } else {
-        Err(anyhow::anyhow!("Unexpected account type"))
-    }
+    // Extract and decode the account information from the response
+    let account_data = response.value;
+
+    // Deserialize the BaseAccount protobuf message
+    let base_account: BaseAccount = BaseAccount::decode(account_data.as_slice())?;
+    println!("base account: {:?}", base_account);
+
+    // Return the sequence number
+    Ok(base_account.sequence)
 }
