62162c64e392b8989594dd7aba224bcbad9c7a4e
BiPhan4 <biphan374@gmail.com>
2024-08-22 12:23:20 -0400

    can handle 500?

--- sandboxes/queue/src/main.rs
+++ sandboxes/queue/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     // Spawn a producer thread
     let producer_log_file = Arc::clone(&log_file);
     let producer = thread::spawn(move || {
-        for i in 1..=100 {
+        for i in 1..=500 {
             match sender.send(i) {
                 Ok(_) => {
                     println!("Produced: {}", i);
