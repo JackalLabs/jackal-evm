ff0c0202d313308ef756f72543412561ec11a730
BiPhan4 <biphan374@gmail.com>
2024-09-03 19:13:06 -0400

    add factory msg types

---
+++ cosmwasm/contracts/bindings_factory/src/msg.rs
@@ -0,0 +1,27 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub bindings_code_id: u64,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+
+    CallBindings {
+        evm_address: String, // Will use this to find mapped bindings contract address to call 
+        msg:         String, // Just raw JSON? 
+    },
+
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    /// GetContractState returns the contact's state.
+    #[returns(crate::state::ContractState)]
+    GetContractState {},
+    /// GetUserBindingsAddress returns the bindings contract address owned by the given user address
+    #[returns(String)]
+    GetUserBindingsAddress { user_address: String},
+}
