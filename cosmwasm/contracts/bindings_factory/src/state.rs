c8eb63ed58a8cbe33a9154d103b44c72f35909c3
BiPhan4 <biphan374@gmail.com>
2024-09-03 19:03:29 -0400

    add factory state

---
+++ cosmwasm/contracts/bindings_factory/src/state.rs
@@ -0,0 +1,32 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Addr;
+use cw_storage_plus::{Item, Map};
+
+pub use contract::ContractState;
+
+/// The item used for storing the bindings contract's code id 
+/// TODO: need a function to update the code ID when we release an updated version of the outpost 
+pub const STATE: Item<ContractState> = Item::new("state");
+
+/// A mapping of the user's evm address to the bindings contract address they own 
+pub const USER_ADDR_TO_BINDINGS_ADDR: Map<&str, String> = Map::new("user_addr_to_bindings_addr");
+
+mod contract {
+
+    use super::*;
+
+    #[cw_serde]
+    pub struct ContractState {
+        /// The code ID of the bindings contract.
+        pub bindings_code_id: u64,
+    }
+
+    impl ContractState {
+        /// Creates a new ContractState.
+        pub fn new(bindings_code_id: u64) -> Self {
+            Self {
+                bindings_code_id,
+            }
+        }
+    }
+}
