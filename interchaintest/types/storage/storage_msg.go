6d5e5eda986fe09e01f31445fd33f093c58e5e98
BiPhan4 <biphan374@gmail.com>
2024-09-09 21:25:09 -0400

    add go type for storage msgs

---
+++ interchaintest/types/storage/storage_msg.go
@@ -0,0 +1,36 @@
+package storage
+
+// helper functions to create json msgs for CosmWasm instantiate, execute, and migrate
+import (
+	"encoding/json"
+)
+
+// WARNING: I think this needs to be in a folder called 'filetree_types' because mailbox_msg.go will also need an 'ExecuteMsg' enum
+// ExecuteMsg is the message to execute the filetree contract.
+type ExecuteMsg struct {
+	PostFile *ExecuteMsg_PostFile `json:"post_file,omitempty"`
+}
+
+type ExecuteMsg_PostFile struct {
+	Merkle        []byte `json:"merkle"`
+	FileSize      int64  `json:"file_size"`
+	ProofInterval int64  `json:"proof_interval"`
+	ProofType     int64  `json:"proof_type"`
+	MaxProofs     int64  `json:"max_proofs"`
+	Expires       int64  `json:"expires"`
+	Note          string `json:"note"`
+}
+
+// ToString returns a string representation of the message
+func (m *ExecuteMsg) ToString() string {
+	return toString(m)
+}
+
+func toString(v any) string {
+	jsonBz, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(jsonBz)
+}
