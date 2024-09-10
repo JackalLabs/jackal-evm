cd59974e452320a3b52c50618401321178278466
BiPhan4 <biphan374@gmail.com>
2024-09-10 11:52:59 -0400

    use many bindings 0.0.2

--- interchaintest/cosmos_chains_config.go
+++ interchaintest/cosmos_chains_config.go
@@ -94,7 +94,7 @@ var chainSpecs = []*interchaintest.ChainSpec{
 			Images: []ibc.DockerImage{
 				{
 					Repository: "biphan4/many-bindings", // FOR LOCAL IMAGE USE: Docker Image Name
-					Version:    "0.0.1",                 // FOR LOCAL IMAGE USE: Docker Image Tag
+					Version:    "0.0.2",                 // FOR LOCAL IMAGE USE: Docker Image Tag
 				},
 			},
 			Bin:            "canined",
