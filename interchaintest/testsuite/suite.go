73d22e99c7b18dc8aa924778109622d751aeb09d
BiPhan4 <biphan374@gmail.com>
2024-09-09 20:57:56 -0400

    remove un-needed ibc connection

--- interchaintest/testsuite/suite.go
+++ interchaintest/testsuite/suite.go
@@ -2,7 +2,6 @@ package testsuite
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 
@@ -19,7 +18,6 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 	"github.com/strangelove-ventures/interchaintest/v7/relayer"
 	"github.com/strangelove-ventures/interchaintest/v7/testreporter"
-	"github.com/strangelove-ventures/interchaintest/v7/testutil"
 )
 
 type TestSuite struct {
@@ -129,148 +127,6 @@ func (s *TestSuite) SetupSuite(ctx context.Context, chainSpecs []*interchaintest
 	ChainAFaucet, err := interchaintest.GetAndFundTestUserWithMnemonic(ctx, "wasmd", ChainAFaucetSeed, userFundsInt, s.ChainA)
 	s.Require().NoError(err)
 	s.ChainAFaucet = ChainAFaucet
-
-	// Generate a new IBC path
-	err = s.Relayer.GeneratePath(ctx, s.ExecRep, s.ChainA.Config().ChainID, s.ChainB.Config().ChainID, s.PathName)
-	s.Require().NoError(err)
-
-	// Wait for blocks
-	err = testutil.WaitForBlocks(ctx, 4, s.ChainA, s.ChainB)
-	s.Require().NoError(err)
-
-	// Create new clients
-	err = s.Relayer.CreateClients(ctx, s.ExecRep, s.PathName, ibc.CreateClientOptions{TrustingPeriod: "330h"})
-	s.Require().NoError(err)
-
-	err = testutil.WaitForBlocks(ctx, 4, s.ChainA, s.ChainB)
-	s.Require().NoError(err)
-
-	// Wasmd should have a light client that's tracking the state of jackal-1
-	lightClients0, lightClients1 := s.Relayer.GetClients(ctx, s.ExecRep, s.ChainA.Config().ChainID)
-
-	// log first wasmd light client
-	lc0, err := json.MarshalIndent(lightClients0, "", "  ")
-
-	logger.LogInfo("Wasmd First light client is")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed to marshal light client:", err)
-	} else {
-		logger.LogInfo(string(lc0))
-	}
-
-	// log second wasmd light client
-	// note: second object being returned seems to be nil
-	lc1, err := json.MarshalIndent(lightClients1, "", "  ")
-
-	logger.LogInfo("Wasmd Second light client is")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed to marshal light client:", err)
-	} else {
-		logger.LogInfo(string(lc1))
-	}
-
-	// Let's log jackal-1 light clients
-	// jackal-1 should have a light client that's tracking the state of wasmd
-	jackalLC0, jackalLC1 := s.Relayer.GetClients(ctx, s.ExecRep, s.ChainB.Config().ChainID)
-
-	// log first jackal light client
-	jlc0, err := json.MarshalIndent(jackalLC0, "", "  ")
-
-	logger.LogInfo("jackal first light client is")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed to marshal light client:", err)
-	} else {
-		logger.LogInfo(string(jlc0))
-	}
-
-	// log second jackal light client
-	// note: second object being returned seems to be nil
-	jlc1, err := json.MarshalIndent(jackalLC1, "", "  ")
-
-	logger.LogInfo("jackal second light client is")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed to marshal light client:", err)
-	} else {
-		logger.LogInfo(string(jlc1))
-	}
-
-	// Create a new connection
-	err = s.Relayer.CreateConnections(ctx, s.ExecRep, s.PathName)
-	s.Require().NoError(err)
-
-	err = testutil.WaitForBlocks(ctx, 4, s.ChainA, s.ChainB)
-	s.Require().NoError(err)
-
-	// Query for the newly created connections in wasmd
-	wasmdConnections, err := s.Relayer.GetConnections(ctx, s.ExecRep, s.ChainA.Config().ChainID)
-	s.Require().NoError(err)
-
-	// log first wasmd connection
-	wc0JsonBytes, err := json.MarshalIndent(wasmdConnections[0], "", "  ")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed to marshal connection:", err)
-	} else {
-		logger.LogInfo(string(wc0JsonBytes))
-	}
-
-	//log second wasmd connection
-
-	wc1JsonBytes, err := json.MarshalIndent(wasmdConnections[1], "", "  ")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed  to marshal connection:", err)
-	} else {
-		logger.LogInfo(string(wc1JsonBytes))
-	}
-
-	// localhost is always a connection since ibc-go v7.1+
-	s.Require().Equal(2, len(wasmdConnections))
-
-	// additional note: wasmd has 2 established connections but canined only has 1. Need to log.
-
-	wasmdConnection := wasmdConnections[0]
-	s.Require().NotEqual("connection-localhost", wasmdConnection.ID)
-	s.ChainAConnID = wasmdConnection.ID
-
-	// Query for the newly created connections in canined
-	caninedConnections, err := s.Relayer.GetConnections(ctx, s.ExecRep, s.ChainB.Config().ChainID)
-	s.Require().NoError(err)
-
-	// localhost is always a connection since ibc-go v7.1+
-	// but canine-chain is running ibc-go v4.4.2, so perhaps there's only 1 connection that isn't localhost?
-
-	s.Require().Equal(1, len(caninedConnections))
-
-	logger.LogInfo("The first canined connections are:")
-	// log the first canined connection
-	cc1JsonBytes, err := json.MarshalIndent(caninedConnections[0], "", "  ")
-
-	if err != nil {
-		// handle error
-		logger.LogError("failed  to marshal connection:", err)
-	} else {
-		logger.LogInfo(string(cc1JsonBytes))
-	}
-
-	caninedConnection := caninedConnections[0]
-
-	s.Require().NotEqual("connection-localhost", caninedConnection.ID)
-	s.ChainBConnID = caninedConnection.ID
-
-	// Start the relayer and set the cleanup function.
-	err = s.Relayer.StartRelayer(ctx, s.ExecRep, s.PathName)
-	s.Require().NoError(err)
 
 	// NOTE: not really sure where to pass this in atm
 	usingPorts := nat.PortMap{}
