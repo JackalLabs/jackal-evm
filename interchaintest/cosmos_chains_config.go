33142d8e23b5e20570fd1f6673608f3efde4ebc4
BiPhan4 <biphan374@gmail.com>
2025-02-04 20:02:37 -0500

    use updated canined image

--- interchaintest/cosmos_chains_config.go
+++ interchaintest/cosmos_chains_config.go
@@ -70,7 +70,7 @@ var chainSpecs = []*interchaintest.ChainSpec{
 			Images: []ibc.DockerImage{
 				{
 					Repository: "biphan4/canine-evm", // FOR LOCAL IMAGE USE: Docker Image Name
-					Version:    "0.0.1",              // FOR LOCAL IMAGE USE: Docker Image Tag
+					Version:    "0.0.2",              // FOR LOCAL IMAGE USE: Docker Image Tag
 				},
 			},
 			Bin:            "canined",
